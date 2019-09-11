333f05e8ecc2f8dbebf21e4610bcf135537c548e
noura <noura.aldubaikhai@hotmail.com>
2019-09-11 11:46:20 +0300

    final update for websiite

--- project/js/main.js
+++ project/js/main.js
@@ -19,3 +19,11 @@ var dots = document.getElementsByClassName("dot");
     dots[slideIndex-1].className += " active";
     setTimeout(showSlides, 7000); 
 }
+
+var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+    if(!isChrome){
+      $('#iframeAudio').remove()
+    }
+  else{
+     $('#playAudio').remove() //just to make sure that it will not have 2x audio in the background 
+  }
